9284fe54a0e3bf1268c80ae8883ac7b32d1f143d
Muhammad-1Haseeb <muhammadhaseeb24@gmail.com>
2026-09-07 16:38:44 +0500

    encryption and decryption

---
+++ 12_encryption.py
@@ -0,0 +1,35 @@
+import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+# print(f"chars: {chars}")
+# print(f"key: {key}")
+
+
+# Encryption 
+plain_text = input("Enter the text to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"Original msg: {plain_text}")
+print(f"Encrypted msg: {cipher_text}")
+
+# Decryption
+cipher_text = input("Enter the text to decrypt: ")
+plain_text = "" 
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"Decrypted msg: {plain_text}")
+print(f"Encrypted msg: {cipher_text}")
