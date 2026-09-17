7d3e8b215bdd6433fd945dddc133090ceec3758f
Muhammad-1Haseeb <muhammadhaseeb24@gmail.com>
2026-09-18 00:45:46 +0500

    heart shape

--- 17_impressive.py
+++ 17_impressive.py
@@ -5,5 +5,5 @@ for y in range(15, -15, -1):
         X = x / 15
         Y = y / 10
         f = (X**2 + Y**2 - 1)**3 - X**2 * Y**3  # Fixed operator here
-        print("*" if f <= 0 else " ", end="")
+        print("*" if f <= 0 else " ", end=" ")
     print()
