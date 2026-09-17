74130f926806f29591fb3ecde112f61e1b822ba4
Muhammad-1Haseeb <muhammadhaseeb24@gmail.com>
2026-09-18 00:45:10 +0500

    hear shape

---
+++ 17_impressive.py
@@ -0,0 +1,9 @@
+import math
+
+for y in range(15, -15, -1):
+    for x in range(-30, 30):
+        X = x / 15
+        Y = y / 10
+        f = (X**2 + Y**2 - 1)**3 - X**2 * Y**3  # Fixed operator here
+        print("*" if f <= 0 else " ", end="")
+    print()
